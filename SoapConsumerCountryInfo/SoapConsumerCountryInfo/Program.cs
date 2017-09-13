4b9982aac72c44f559414dd2bcc2e823448f4e41
andersbor <anbo@easj.dk>
2017-09-13 10:07:09 +0200

    get SOAP, parse XML

---
+++ SoapConsumerCountryInfo/SoapConsumerCountryInfo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using SoapConsumerCountryInfo.ServiceReferenceCountry;
+
+namespace SoapConsumerCountryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (countrySoapClient client = new countrySoapClient("countrySoap"))
+            {
+                string countriesXml = client.GetCountries();
+                Console.WriteLine(countriesXml);
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.LoadXml(countriesXml);
+                XmlNodeList namesXmlList = xmlDocument.GetElementsByTagName("Name");
+                for (int i = 0; i < namesXmlList.Count; i++)
+                {
+                    string countryName = namesXmlList[i].InnerText;
+                    Console.WriteLine(countryName);
+                }
+            }
+        }
+    }
+}
